c1ee1d3608268dd5df0f0405de857681193e211f
eulaussa <ozcsel.16@gmail.com>
2018-12-06 17:25:26 +0300

    Multiple clients can connect
    - Name problem fixed
    - When a client quits, it's not shown in the server side (problem)

--- Step2/Server/server/Form1.Designer.cs
+++ Step2/Server/server/Form1.Designer.cs
@@ -32,6 +32,9 @@
             this.connectButton = new System.Windows.Forms.Button();
             this.description = new System.Windows.Forms.Label();
             this.port = new System.Windows.Forms.TextBox();
+            this.turnNum = new System.Windows.Forms.TextBox();
+            this.description2 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // feed
@@ -44,18 +47,18 @@
             // 
             // connectButton
             // 
-            this.connectButton.Location = new System.Drawing.Point(76, 210);
+            this.connectButton.Location = new System.Drawing.Point(64, 100);
             this.connectButton.Name = "connectButton";
             this.connectButton.Size = new System.Drawing.Size(145, 58);
             this.connectButton.TabIndex = 1;
-            this.connectButton.Text = "Open";
+            this.connectButton.Text = "Listen";
             this.connectButton.UseVisualStyleBackColor = true;
             this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
             // 
             // description
             // 
             this.description.AutoSize = true;
-            this.description.Location = new System.Drawing.Point(25, 107);
+            this.description.Location = new System.Drawing.Point(25, 55);
             this.description.Name = "description";
             this.description.Size = new System.Drawing.Size(127, 17);
             this.description.TabIndex = 2;
@@ -63,22 +66,52 @@
             // 
             // port
             // 
-            this.port.Location = new System.Drawing.Point(168, 107);
+            this.port.Location = new System.Drawing.Point(168, 52);
             this.port.Name = "port";
             this.port.Size = new System.Drawing.Size(85, 22);
             this.port.TabIndex = 3;
+            // 
+            // turnNum
+            // 
+            this.turnNum.Location = new System.Drawing.Point(168, 187);
+            this.turnNum.Name = "turnNum";
+            this.turnNum.Size = new System.Drawing.Size(85, 22);
+            this.turnNum.TabIndex = 5;
+            // 
+            // description2
+            // 
+            this.description2.AutoSize = true;
+            this.description2.Location = new System.Drawing.Point(25, 187);
+            this.description2.Name = "description2";
+            this.description2.Size = new System.Drawing.Size(118, 17);
+            this.description2.TabIndex = 4;
+            this.description2.Text = "Number of turns: ";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(64, 242);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(145, 58);
+            this.startButton.TabIndex = 6;
+            this.startButton.Text = "Start Game";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(608, 363);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.turnNum);
+            this.Controls.Add(this.description2);
             this.Controls.Add(this.port);
             this.Controls.Add(this.description);
             this.Controls.Add(this.connectButton);
             this.Controls.Add(this.feed);
             this.Name = "Form1";
             this.Text = "Server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,6 +123,9 @@
         private System.Windows.Forms.Button connectButton;
         private System.Windows.Forms.Label description;
         private System.Windows.Forms.TextBox port;
+        private System.Windows.Forms.TextBox turnNum;
+        private System.Windows.Forms.Label description2;
+        private System.Windows.Forms.Button startButton;
     }
 }
 
--- Step2/Server/server/Form1.cs
+++ Step2/Server/server/Form1.cs
@@ -16,10 +16,10 @@ namespace server
     public partial class Form1 : Form
     {
         static Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        static List<Socket> clientSockets = new List<Socket>(2);
-        static List<string> names = new List<string>(2) { "", "" };
-        static bool terminating = false, accept = true, turn = true, gameStart = false;
-        static string name1, question, answer, guess;
+        static List<Socket> clientSockets = new List<Socket>();
+        static List<string> names = new List<string>();
+        static bool terminating = false, accept = true;
+        static string question, answer, guess;
         static int clients = 0;
         static readonly object _game = new object(), _accept = new object();
 
@@ -30,11 +30,22 @@ namespace server
             CheckForIllegalCrossThreadCalls = false;
         }
 
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.Visible = false;
+            int turn = int.Parse(turnNum.Text);
+
+            play();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
         private void connectButton_Click(object sender, EventArgs e)
         {
-
+            connectButton.Visible = false;
             int portnum = int.Parse(port.Text);
-
             try
             {
                 //Opening the server 
@@ -55,18 +66,18 @@ namespace server
         //Initiates the game
         private void play()
         {
-            string command = "Ask a question.";
-                Byte[] buffer = Encoding.Default.GetBytes(command);
-                Byte[] a_q_buffer = new Byte[64];
-                Byte[] g_buffer = new Byte[64];
+            /*string command = "Ask a question.";
+            Byte[] buffer = Encoding.Default.GetBytes(command);
+            Byte[] a_q_buffer = new Byte[64];
+            Byte[] g_buffer = new Byte[64];
             //turn = true for the first client's turn, false for second client's turn
-                if (turn)
+            if (turn)
+            {
+                if (!terminating)
                 {
-                    if (!terminating)
+                    try
                     {
-                        try
-                        {
-                    //Sending the question to first client, waiting for the answer and comparing it to second client's 
+                        //Sending the question to first client, waiting for the answer and comparing it to second client's 
                         clientSockets[0].Send(buffer);
                         feed.Text += "Waiting for the question." + '\n';
                         clientSockets[0].Receive(a_q_buffer);
@@ -102,9 +113,9 @@ namespace server
                                 turn = false;
                             }
                         }
-                    
-                }
-                //if a clients is disconected from the server, game stops and all sockets are closed
+
+                    }
+                    //if a clients is disconected from the server, game stops and all sockets are closed
                     catch
                     {
                         if (!terminating)
@@ -112,91 +123,86 @@ namespace server
                             feed.Text += "Client 1 has disconnected. Game will stop. \n";
                             terminating = true;
                         }
-                        
+
                     }
                 }
-                    
-                    
-                }
-                else
+
+
+            }
+            else
+            {
+                if (!terminating)
                 {
-                    if (!terminating)
+                    try
                     {
-                        try
-                        {
-                            //turn of second client, question is sent to first client and compared with first one's answer
-                            
+                        //turn of second client, question is sent to first client and compared with first one's answer
 
-                            clientSockets[1].Send(buffer);
-                            feed.Text += "Waiting for the question" + '\n';
-                            clientSockets[1].Receive(a_q_buffer);                            
-                            string a_q = Encoding.Default.GetString(a_q_buffer);
-                            if (a_q.Substring(0, a_q.IndexOf('?') + 1) != "")
+
+                        clientSockets[1].Send(buffer);
+                        feed.Text += "Waiting for the question" + '\n';
+                        clientSockets[1].Receive(a_q_buffer);
+                        string a_q = Encoding.Default.GetString(a_q_buffer);
+                        if (a_q.Substring(0, a_q.IndexOf('?') + 1) != "")
+                        {
+                            question = a_q.Substring(0, a_q.IndexOf('?') + 1);
+                            feed.Text += "Question: " + question + '\n';
+                            answer = a_q.Substring(a_q.IndexOf('?') + 1, a_q.IndexOf('.'));
+                            clientSockets[0].Send(Encoding.Default.GetBytes(question));
+                            feed.Text += "Waiting for a guess. \n";
+                            clientSockets[0].Receive(g_buffer);
+                            if (guess.Substring(0, guess.IndexOf('.') + 1) != "")
                             {
-                                question = a_q.Substring(0, a_q.IndexOf('?') + 1);
-                                feed.Text += "Question: " + question + '\n';
-                                answer = a_q.Substring(a_q.IndexOf('?') + 1, a_q.IndexOf('.'));
-                                clientSockets[0].Send(Encoding.Default.GetBytes(question));
-                                feed.Text += "Waiting for a guess. \n";
-                                clientSockets[0].Receive(g_buffer);
-                                if (guess.Substring(0, guess.IndexOf('.') + 1) != "")
+                                guess = Encoding.Default.GetString(g_buffer);
+                                feed.Text += "Guess: " + guess.Substring(0, guess.IndexOf('.') + 1) + '\n';
+                                feed.Text += "Answer: " + answer.Substring(0, answer.IndexOf('.') + 1) + '\n';
+                                if (guess.Substring(0, guess.IndexOf('.') + 1) == answer.Substring(0, answer.IndexOf('.') + 1))
                                 {
-                                    guess = Encoding.Default.GetString(g_buffer);
-                                    feed.Text += "Guess: " + guess.Substring(0, guess.IndexOf('.') + 1) + '\n';
-                                    feed.Text += "Answer: " + answer.Substring(0, answer.IndexOf('.') + 1) + '\n';
-                                    if (guess.Substring(0, guess.IndexOf('.') + 1) == answer.Substring(0, answer.IndexOf('.') + 1))
-                                    {
-                                        feed.Text += "Correct! \n";
-                                        clientSockets[0].Send(Encoding.Default.GetBytes("1"));
-                                        clientSockets[1].Send(Encoding.Default.GetBytes("1"));
-                                    }
-                                    // if false
-                                    else
-                                    {
-                                        feed.Text += "Wrong! \n";
-
-                                        clientSockets[0].Send(Encoding.Default.GetBytes("0"));
-                                        clientSockets[1].Send(Encoding.Default.GetBytes("0"));
-                                    }
-                                    turn = true;
+                                    feed.Text += "Correct! \n";
+                                    clientSockets[0].Send(Encoding.Default.GetBytes("1"));
+                                    clientSockets[1].Send(Encoding.Default.GetBytes("1"));
                                 }
-                            }
-                            //if answer is true
-                            
-                        }
-                        catch
-                        {
-                            //if one of the clients disconnects from the server, the game stops and everything is closed down.
-                            if (!terminating)
-                            {
-                                feed.Text += "Client 2 has disconnected. Game will stop. \n";
-                                terminating = true;
-                            }
+                                // if false
+                                else
+                                {
+                                    feed.Text += "Wrong! \n";
 
+                                    clientSockets[0].Send(Encoding.Default.GetBytes("0"));
+                                    clientSockets[1].Send(Encoding.Default.GetBytes("0"));
+                                }
+                                turn = true;
+                            }
                         }
+                        //if answer is true
+
                     }
-                  
+                    catch
+                    {
+                        //if one of the clients disconnects from the server, the game stops and everything is closed down.
+                        if (!terminating)
+                        {
+                            feed.Text += "Client 2 has disconnected. Game will stop. \n";
+                            terminating = true;
+                        }
+
+                    }
                 }
 
+            }
+            */
         }
-
-       
         //Accepting clients
         private void Accept(object obj)
-        {          
+        {
             //Accepting client
             while (accept)
             {
-                
                 try
                 {
                     Socket newClient = serverSocket.Accept();
                     clientSockets.Add(newClient);
-
                     Thread receiveThread = new Thread(Receive);
-                   
                     receiveThread.Start();
-                    
+
                 }
                 catch
                 {
@@ -222,80 +228,38 @@ namespace server
             while (connected && !terminating)
             {
                 int lenClientSoc = clientSockets.Count();
-                if (names[lenClientSoc - 1] == "" || gameStart)
+
+                Socket thisClient = clientSockets[lenClientSoc - 1];
+                try
                 {
-                    lock (_game)
+                    Byte[] buffer = new Byte[500];
+                    thisClient.Receive(buffer);
+                    string name = Encoding.Default.GetString(buffer).Replace("\0", string.Empty);
+                    while (names.Any(item => item == name))
                     {
-                        Socket thisClient = clientSockets[lenClientSoc - 1];
-
-                        if (!gameStart)
-                        {
-                            //unitl both of the names are different from each other, server asks for a different name from second client
-
-                            try
-                            {
-                                Byte[] buffer = new Byte[64];
-                                thisClient.Receive(buffer);
-                                if (clients != 2)
-                                {
-                                    name1 = Encoding.Default.GetString(buffer);
-                                }
-                                clients++;
-
-                                    if (names[0] == name1)
-                                    {
-                                        while (name1 == names[0])
-                                        {
-                                            thisClient.Send(Encoding.Default.GetBytes("Please enter a different name."));
-                                            thisClient.Receive(buffer);
-                                            name1 = Encoding.Default.GetString(buffer);
-                                        }
-                                    }
-                                
-                                // when both of them have different names, connection to both of the clients established 
-                                feed.Text += name1;
-                                feed.Text += " has connected. \n";
-                                names[lenClientSoc - 1] = name1;
-                            }
-                            catch
-                            {
-                                //when one of the clients disconencts
-                                connected = false;
-                                if (!terminating)
-                                {
-                                    feed.Text += "Client has disconnected" + '\n';
-                                }
-
-                            }
-                        }
-
-                        //when both of the clients connect, game starts
-                        if (clients == 2 && !gameStart)
-                        {
-                            gameStart = true;
-                        }
-                        else if (gameStart)
-                        {
-
-                            if (gameStart)
-                            {
-                                if (String.Compare(names[0], names[1]) == 1)
-                                {
-                                    string temp = names[0];
-                                    names[0] = names[1];
-                                    names[1] = temp;
-
-                                    Socket tmp = clientSockets[0];
-                                    clientSockets[0] = clientSockets[1];
-                                    clientSockets[1] = tmp;
-                                }
-                                play();
-                            }
-                        }
+                        buffer = new Byte[500];
+                        thisClient.Send(Encoding.Default.GetBytes("Please enter a different name."));
+                        thisClient.Receive(buffer);
+                        name = Encoding.Default.GetString(buffer).Replace("\0", string.Empty);
                     }
+                    feed.Text += name;
+                    feed.Text += " has connected. \n";
+                    names.Add(name);
+
                 }
-                
-                
+                catch
+                {
+                    connected = false;
+                    if (!terminating)
+                    {
+                        feed.Text += names[lenClientSoc - 1];
+                        feed.Text += " has disconnected. \n";
+                    }
+                    thisClient.Close();
+                    clientSockets.Remove(thisClient);
+                }
+
+
             }
         }
 
